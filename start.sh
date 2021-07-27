8114b55b4b5dcd85e445b9c9b65fefeb055e7995
Alex Fan <codetiger927@gmail.com>
2021-07-27 02:01:04 -0400

    simple fix

--- start.sh
+++ start.sh
@@ -129,7 +129,7 @@ function stop_servers {
 
 # run_experiment executes the given experiment
 function run_experiment {
-  sudo /home/alexfan/pegasus-experiment/experiment$expno.sh "$slowdownip"
+  /home/alexfan/pegasus-experiment/experiment$expno.sh "$slowdownip"
 }
 
 # test_run is the main driver function
