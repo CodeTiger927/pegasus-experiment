47ab53e3edfd1d0dc82541343d223e8874177f7e
Alex Fan <codetiger927@gmail.com>
2021-07-27 02:45:47 -0400

    simple fix

---
+++ experiment2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -ex
+
+secondaryip=$1
+
+ssh -i ~/.ssh/id_rsa alexfan@"$secondaryip" "sh -c 'nohup taskset -ac 0 /home/alexfan/pegasus-experiment/deadloop > /dev/null 2>&1 &'"
+deadlooppid=$(ssh -i ~/.ssh/id_rsa alexfan@"$secondaryip" "sh -c 'pgrep deadloop'")
+ssh -i ~/.ssh/id_rsa alexfan@"$secondaryip" "sudo sh -c 'sudo mkdir /sys/fs/cgroup/cpu/cpulow /sys/fs/cgroup/cpu/cpuhigh'"
+ssh -i ~/.ssh/id_rsa alexfan@"$secondaryip" "sudo sh -c 'sudo echo 64 > /sys/fs/cgroup/cpu/cpulow/cpu.shares'"
+ssh -i ~/.ssh/id_rsa alexfan@"$secondaryip" "sudo sh -c 'sudo echo $deadlooppid > /sys/fs/cgroup/cpu/cpuhigh/cgroup.procs'"
+ssh -i ~/.ssh/id_rsa alexfan@"$secondaryip" "ps -ef | awk '/[p]egasus/{print \$2}'>tmpoutput"
+ssh -i ~/.ssh/id_rsa alexfan@"$secondaryip" "sudo sh -c 'while IFS= read -r line; do sudo echo \$line>>/sys/fs/cgroup/cpu/cpulow/cgroup.procs; done<tmpoutput'"
---
+++ experiment3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -ex
+
+secondaryip=$1
+
+ssh -i ~/.ssh/id_rsa alexfan@"$secondaryip" "sudo sh -c 'sudo mkdir /sys/fs/cgroup/blkio/db'"
+ssh -i ~/.ssh/id_rsa alexfan@"$secondaryip" "sudo sh -c 'sync; echo 3 > /proc/sys/vm/drop_caches'"
+lsblkcmd="8:32 524288"
+ssh -i ~/.ssh/id_rsa alexfan@"$secondaryip" "sudo sh -c 'sudo echo $lsblkcmd > /sys/fs/cgroup/blkio/db/blkio.throttle.read_bps_device'"                 
+ssh -i ~/.ssh/id_rsa alexfan@"$secondaryip" "sudo sh -c 'sudo echo $lsblkcmd > /sys/fs/cgroup/blkio/db/blkio.throttle.write_bps_device'"                                                                                                                         
+ssh -i ~/.ssh/id_rsa alexfan@"$secondaryip" "ps -ef | awk '/[p]egasus/{print \$2}'>tmpoutput"
+ssh -i ~/.ssh/id_rsa alexfan@"$secondaryip" "sudo sh -c 'while IFS= read -r line; do sudo echo \$line>>/sys/fs/cgroup/cpu/cpulow/cgroup.procs; done<tmpoutput'"
