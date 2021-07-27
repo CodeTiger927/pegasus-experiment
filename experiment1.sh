9e33f45bff187e61173df5e30e9f26f28eb308d1
Alex Fan <codetiger927@gmail.com>
2021-07-27 06:19:26 +0000

    stuff

--- experiment1.sh
+++ experiment1.sh
@@ -8,4 +8,4 @@ ssh -i ~/.ssh/id_rsa alexfan@"$secondaryip" "sudo sh -c 'sudo mkdir /sys/fs/cgro
 ssh -i ~/.ssh/id_rsa alexfan@"$secondaryip" "sudo sh -c 'sudo echo 500000 > /sys/fs/cgroup/cpu/db/cpu.cfs_quota_us'"
 ssh -i ~/.ssh/id_rsa alexfan@"$secondaryip" "sudo sh -c 'sudo echo 1000000 > /sys/fs/cgroup/cpu/db/cpu.cfs_period_us'"
 ssh -i ~/.ssh/id_rsa alexfan@"$secondaryip" "ps -ef | awk '/[p]egasus/{print \$2}'>tmpoutput"
-ssh -i ~/.ssh/id_rsa alexfan@"$secondaryip" "sudo sh -c '\''while IFS= read -r line; do sudo echo $line>>/sys/fs/cgroup/cpu/db/cgroup.procs; done<tmpoutput'"
+ssh -i ~/.ssh/id_rsa alexfan@"$secondaryip" "sudo sh -c 'while IFS= read -r line; do sudo echo $line>>/sys/fs/cgroup/cpu/db/cgroup.procs; done<tmpoutput'"
