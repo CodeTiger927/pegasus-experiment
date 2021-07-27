8a805aa57807c368aa0a05924d25e8ddddb0ca09
Alex Fan <codetiger927@gmail.com>
2021-07-27 01:44:37 -0400

    idk

--- experiment1.sh
+++ experiment1.sh
@@ -7,4 +7,5 @@ secondaryip=$1
 ssh -i ~/.ssh/id_rsa alexfan@"$secondaryip" "sudo sh -c 'sudo mkdir /sys/fs/cgroup/cpu/db'"
 ssh -i ~/.ssh/id_rsa alexfan@"$secondaryip" "sudo sh -c 'sudo echo 500000 > /sys/fs/cgroup/cpu/db/cpu.cfs_quota_us'"
 ssh -i ~/.ssh/id_rsa alexfan@"$secondaryip" "sudo sh -c 'sudo echo 1000000 > /sys/fs/cgroup/cpu/db/cpu.cfs_period_us'"
-ssh -i ~/.ssh/id_rsa alexfan@"$secondaryip" sudo sh -c "ps -ef | awk '/[p]egasus/{print \$2}'>/sys/fs/cgroup/cpu/db/cgroup.procs"
+ssh -i ~/.ssh/id_rsa alexfan@"$secondaryip" "ps -ef | awk '/[p]egasus/{print \$2}'>tmpoutput"
+ssh -i ~/.ssh/id_rsa alexfan@"$secondaryip" "sudo sh -c 'cat tmpoutput>/sys/fs/cgroup/cpu/db/cgroup.procs'"
